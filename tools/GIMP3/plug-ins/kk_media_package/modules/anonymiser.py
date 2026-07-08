8d9add2831a9a64aec6cf0614f74891de2251d23
Konrad Keck <nigma-mania@web.de>
2026-07-09 00:24:34 +0200

    Refactor placeholder functions in anonymiser.py for improved clarity and consistency

--- tools/GIMP3/plug-ins/kk_media_package/modules/anonymiser.py
+++ tools/GIMP3/plug-ins/kk_media_package/modules/anonymiser.py
@@ -3,5 +3,25 @@
 
 from gi.repository import Gimp
 
+def black_bar(image, drawable):
+    # this function will introduce a black bar in a seperate layer at a selected position, with a selected size and opacity. The user will be able to select the position, size and opacity of the black bar (with defaults & smart values).
+    # todos:
+    ## default values: mid of image, default size: 1% of image height 3x1 dimensions, default opacity: 100%, color of bar: black, default orientation: horizontal
+    
+    Gimp.message("Function kk-black-bar is a placeholder.")
+
+def pixeled(image, drawable):
+    # this function will pixelate a selected area in a seperate layer at a selected position, with a selected size and pixelation level. The user will be able to select the position, size and pixelation level (with defaults & smart values).
+    # todos:
+    ## default values: mid of image, default size: 2% of image height 4x5 dimensions, default opacity: 100%, default orientation: portrait, default pixelation level: 10px
+    Gimp.message("Function kk-pixeled is a placeholder.")
+
+def blurred(image, drawable):
+    # this function will blur a selected area in a seperate layer at a selected position, with a selected size and blur level. The user will be able to select the position, size and blur level (with defaults & smart values).
+    # todos:
+    ## default values: mid of image, default size: 2% of image height 4x5 dimensions, default opacity: 100%, default orientation: portrait, default blur level: 10px
+    Gimp.message("Function kk-blurred is a placeholder.")
+
+
 def anonymiser(image, drawable):
-    Gimp.message("Function python-fu-anonymiser is a placeholder.")
+    Gimp.message("Function kk-anonymiser is a placeholder.")
